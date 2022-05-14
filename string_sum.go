5bbda58c3df8f4666d0344843d0b9fb9b732441c
m-tretyak <m.tretyak@gmail.com>
2022-05-14 05:14:04 +0300

    HW40

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,8 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -10,6 +12,7 @@ var (
 	errorEmptyInput = errors.New("input is empty")
 	// Use when the expression has number of operands not equal to two
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+	errorInvalidOperand = errors.New("invalid operand, cannot parse no more")
 )
 
 // Implement a function that computes the sum of two int numbers written as a string
@@ -22,6 +25,76 @@ var (
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
-func StringSum(input string) (output string, err error) {
-	return "", nil
+func getOperands(input string) ([]string, error) {
+	result := make([]string, 0, 2)
+	operand := []rune{'+'}
+	for _, r := range input + " " {
+		switch len(operand) {
+		case 0:
+			switch r {
+			case ' ':
+				continue
+			case '+', '-':
+				operand = append(operand, r)
+				continue
+			default:
+				return []string{}, fmt.Errorf("%q : %w", r, errorInvalidOperand)
+			}
+		case 1:
+			switch r {
+			case ' ':
+				continue
+			case '+', '-':
+				operand[0] = r
+				continue
+			default:
+				operand = append(operand, r)
+			}
+		default:
+			switch r {
+			case ' ':
+				result = append(result, string(operand))
+				operand = []rune{}
+				continue
+			case '+', '-':
+				result = append(result, string(operand))
+				operand = []rune{r}
+				continue
+			default:
+				operand = append(operand, r)
+			}
+		}
+	}
+
+	return result, nil
+}
+
+func StringSum(input string) (string, error) {
+	if len(input) == 0 {
+		return "", fmt.Errorf("%w", errorEmptyInput)
+	}
+
+	operands, err := getOperands(input)
+	if len(operands) != 2 {
+		return "", fmt.Errorf("%w", errorNotTwoOperands)
+	}
+
+	var result, num = 0, 0
+	for _, operand := range operands {
+		if num, err = idiots_written_tests(operand); err != nil {
+			return "", fmt.Errorf("failed to convert %q: %w", operand, err)
+		}
+
+		result += num
+	}
+
+	return strconv.Itoa(result), nil
+}
+
+func idiots_written_tests(operand string) (int, error) {
+	if operand[0] == '-' {
+		return strconv.Atoi(operand)
+	}
+
+	return strconv.Atoi(operand[1:])
 }
